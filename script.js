ed050095ac777694fc7968ff52e02c5815e47c44
leandro brito <leandroabrito89@gmail.com>
2020-07-14 19:57:28 -0300

    populate ui with localstorage data

--- script.js
+++ script.js
@@ -2,23 +2,15 @@ const container = document.querySelector('.container');
 const seats = document.querySelectorAll('.row .seat:not(.occupied)');
 const count = document.getElementById('count');
 const total = document.getElementById('total');
-
 const movieSelect = document.getElementById('movie');
 
+populateUI();
 // const ticketPrice = parseInt(movieSelect.value);
 let ticketPrice = +movieSelect.value;
 // console.log(typeof ticketPrice);
 
 // container.addEventListener('click', function(e) {
 // })
-
-
-// save selected movie index and price
-function setMovieData(movieIndex, moviePrice) {
-  localStorage.setItem('selectedMovieIndex', movieIndex);
-  localStorage.setItem('selectedMoviePrice', moviePrice);
-}
-
 
 // update total and count
 function updateSelectedCount() {
@@ -31,6 +23,30 @@ function updateSelectedCount() {
   const selectedSeatsCount = selectedSeats.length;
   count.innerText = selectedSeatsCount;
   total.innerText = selectedSeatsCount * ticketPrice;
+}
+
+// save selected movie index and price
+function setMovieData(movieIndex, moviePrice) {
+  localStorage.setItem('selectedMovieIndex', movieIndex);
+  localStorage.setItem('selectedMoviePrice', moviePrice);
+}
+
+// get data from local storage and populate UI
+function populateUI() {
+  const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+  if(selectedSeats !== null && selectedSeats.length > 0) {
+    seats.forEach((seat, index) => {
+      if(selectedSeats.indexOf(index) > -1) {
+        seat.classList.add('selected');
+      }
+    });
+  }
+
+  const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+
+  if(selectedMovieIndex !== null) {
+    movieSelect.selectedIndex = selectedMovieIndex;
+  }
 }
 
 
@@ -51,6 +67,15 @@ container.addEventListener('click', (e) => {
 
     updateSelectedCount();
   }
-})
+});
+
+// initial count and total set
+updateSelectedCount();
+
+
+
+
+
+
 
 
