1c2edf4f56addf6fbc45af5788b2ea18660f9dea
leandro brito <leandroabrito89@gmail.com>
2020-07-14 19:14:02 -0300

    select movie and seats from ui and process info

---
+++ script.js
@@ -0,0 +1,43 @@
+const container = document.querySelector('.container');
+const seats = document.querySelectorAll('.row .seat:not(.occupied)');
+const count = document.getElementById('count');
+const total = document.getElementById('total');
+
+const movieSelect = document.getElementById('movie');
+
+// const ticketPrice = parseInt(movieSelect.value);
+let ticketPrice = +movieSelect.value;
+// console.log(typeof ticketPrice);
+
+// container.addEventListener('click', function(e) {
+// })
+
+function updateSelectedCount() {
+  const selectedSeats = document.querySelectorAll('.row .seat.selected');
+  // console.log(selectedSeats);
+  const selectedSeatsCount = selectedSeats.length;
+  // console.log(selectedSeatsCount);
+  count.innerText = selectedSeatsCount;
+  total.innerText = selectedSeatsCount * ticketPrice;
+}
+
+
+movieSelect.addEventListener('change', e => {
+  ticketPrice = +e.target.value;
+  updateSelectedCount();
+});
+
+container.addEventListener('click', (e) => {
+  // console.log(e.target);
+  if (
+    e.target.classList.contains('seat') &&
+    !e.target.classList.contains('occupied')
+  ) {
+    // console.log(e.target);
+    e.target.classList.toggle('selected');
+
+    updateSelectedCount();
+  }
+})
+
+
